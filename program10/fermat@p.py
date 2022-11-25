3e66565802e99f8620114fc18879ea2c4831833b
Prasann <83489924+Prasannkumar202106@users.noreply.github.com>
2022-11-25 23:28:03 +0530

    Create fermat@p.py

---
+++ program10/fermat@p.py
@@ -0,0 +1,13 @@
+#write a program to cechk Primality Test using Fermat method #
+import random
+def fermat(n):#start the fermat function
+    A = random.randint(1, n-1) #  A can have random number from 1 to p-1
+    if(pow(A, n-1)%n != 1):
+        return False
+    return True  #else condition
+x= int(input("enter the number  for check wheather it is prime or not \n") )
+z=fermat(x)      
+if z == True:
+    print("{} is a prime ".format(x))
+elif z==False:
+    print("{} is not prime".format(x))
